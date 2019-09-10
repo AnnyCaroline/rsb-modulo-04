8925d4a24086936bd0a33396b498a4550c6118d5
AnnyCaroline <annycarolinegnr@gmail.com>
2019-09-10 15:33:59 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+```
+yarn init -y
+yarn add @babel/core @babel/preset-end @babel/preset-react webpack webpack-cli -D
+yarn add react react-dom
+yarn add babel-loader -D
+yarn add webpack-dev-server -D
+```
+
+o --mode development deixa o arquivo bundle um pouco mais legível. Com o --mode production fica ilegível, mas mais leve.
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'), //resolve problema com barras no windows
+    output: { // onde joga o bundle. nosso único arquivo js
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
